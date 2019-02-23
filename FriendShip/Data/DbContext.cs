3318414b14dbde6247c102b1b1aa0bcfef38d0f1
Chelsea Aquino <aquinoc2@go.stockton.edu>
2019-02-23 17:09:14 -0500

    Add project files.

---
+++ FriendShip/Data/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TheFriendShip.Models;
+
+namespace FriendShip.Data
+{
+    public class FriendShipDbContext : IdentityDbContext<User>
+    {
+        public FriendShipDbContext(DbContextOptions<FriendShipDbContext> options) : base(options)
+        {
+        }
+        //public DbSet<FriendShip.Models.Product> Product { get; set; }
+        public DbSet<User> Review { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+    }
+}
---
+++ FriendShip/Models/login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendShip.Models
+{
+    public class LoginVM
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+    public class LoginReturn
+    {
+        public string user { get; set; }
+        public string tokenString { get; set; }
+    }
+}
